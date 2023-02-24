3bf93bce3410633a5c67fd830e08f0d55472ec86
roman k <kolomrs@yandex.ru>
2023-02-24 14:50:16 +0300

    add visual tool, fix screens

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
+        'linebreak-style': 0,
     },
     globals: {
         __IS_DEV__: true,
