dac3807ee6675c5af8e79f15dd8d02e709b5172d
roman k <kolomrs@yandex.ru>
2023-03-09 12:35:58 +0300

    add login module

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,7 @@ module.exports = {
                 ignoreAttribute: ['data-testid', 'to'],
             },
         ],
-        'max-len': ['error', { ignoreComments: true, code: 100 }],
+        'max-len': ['error', { ignoreComments: true, code: 110 }],
         'linebreak-style': 0,
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
